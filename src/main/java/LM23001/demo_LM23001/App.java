4d23d750eb6e2138136d42885a5ce6f9ee84127f
Gens <167649608+Gens-Darkspine@users.noreply.github.com>
2024-05-03 11:14:15 -0600

    Add files via upload

---
+++ src/main/java/LM23001/demo_LM23001/App.java
@@ -0,0 +1,16 @@
+package LM23001.demo_LM23001;
+
+public class App 
+{
+	
+	public String Hello() {
+		return "Sonic Heroes";
+	}
+	
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+        System.out.println(hw.Hello());
+    }
+    
+}
---
+++ src/test/java/LM23001/demo_LM23001/AppTest.java
@@ -0,0 +1,20 @@
+package LM23001.demo_LM23001;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello()
+    {
+        assertTrue( true );
+    }
+}
